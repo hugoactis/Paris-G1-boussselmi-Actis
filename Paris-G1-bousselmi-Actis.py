476b8f980a54762d5782e8eaf35b62855454f85f
hugoactis <hugo.actis@essca.eu>
2023-02-27 17:27:04 +0100

    Update Paris-G1-bousselmi-Actis.py
    J'importe les patchs

--- Paris-G1-bousselmi-Actis.py
+++ Paris-G1-bousselmi-Actis.py
@@ -13,3 +13,14 @@ print("test with branch")
 
 
 print("on ira dormir moins bête")
+
+import random
+import yfinance as yf
+import math
+import numpy as np
+import numpy.random as npr
+from pylab import plt, mpl
+import pandas as pd
+import scipy.optimize as sco
+from pandas_datareader import data as pdr
+import scipy.interpolate as sci
